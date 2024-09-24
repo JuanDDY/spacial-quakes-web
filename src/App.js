704bcd91945940a29153f22008df0ea0d5ac8c8d
Juan David Duarte <j.duartey@uniandes.edu.co>
2024-09-23 23:14:05 -0500

    Primeros cambios y pruebas iniciales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GlobeComponent from './components/GlobeComponent'; // Asegúrate de que la ruta es correcta
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Globo 3D de la Luna</h1>
+      <GlobeComponent />
     </div>
   );
 }
 
 export default App;
+
