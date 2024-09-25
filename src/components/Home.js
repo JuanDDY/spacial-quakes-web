5d177aa31989d07c63db27f64d180982d098c679
ikercat2 <118685230+ikercat2@users.noreply.github.com>
2024-09-25 14:30:28 -0500

    new file:   src/components/Home.css modified:   src/components/Home.js modified:   src/components/Home.css modified:   src/components/Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,31 @@
+import './Home.css'; // Asegúrate de crear este archivo CSS para aplicar los estilos
 
+function Home() {
+  return (
+    <div className="home-container">
+      {/* Contenedor de título centrado */}
+      <div className="titulo-container">
+        <h1>Explora y detecta sismos en otros mundos</h1>
+      </div>
 
-function Home () {
+      {/* Contenedor de imágenes centrado */}
+      <div className="centro-imagen">
+        <img src="imagesHome/marte.png" alt="Marte" className="marte-img" />
+        <img src="imagesHome/luna.png" alt="Luna" className="luna-img" />
+        <img src="imagesHome/astronauta.png" alt="Astronauta" className="astronauta-img" />
+      </div>
 
-    return (
-        <div>
+      {/* Botón centrado */}
+      <div className="boton-container">
+        <button className="boton-mision">Empezar misión</button>
+      </div>
 
-            <div class="centro-imagen" > {/* Contenedor relativo para la imagen y el botón de "Me gusta" */}
-                
-                <img src="imagesHome/marte.png"/>
-                <img src="imagesHome/luna.png"/>
-                <img src="imagesHome/astronauta.png"/>
-
-            </div>
-        </div>
-
-
-    );
+      {/* Texto en la parte inferior derecha */}
+      <div className="seisminds-text">
+        SEISMinds
+      </div>
+    </div>
+  );
 }
 
 export default Home;
